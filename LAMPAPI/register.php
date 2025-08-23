6b18238b2f3ff01ab7f5867615bd8930faa3373f
Bruce Kelsey <truechip122@gmail.com>
2025-08-23 16:34:37 -0400

    Adding back-end register for API

---
+++ LAMPAPI/register.php
@@ -0,0 +1,49 @@
+<?php
+	$inData = getRequestInfo();
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$password = $inData["password"];
+
+	$conn = new mysqli("http://178.128.4.96/", "TheShark", "GreatWhite16", "Project1");
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		if($stmt->execute()) {
+			returnWithInfo("Registration successful");
+		} else {
+			returnWithError($stmt->error);
+		}
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $msg )
+	{
+		$retValue = '{"error":"", "message":"' . $msg . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
---
+++ js/register.js
@@ -0,0 +1,36 @@
+function doRegister() {
+
+
+
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let loginName = document.getElementById("loginName").value;
+    let loginPassword = document.getElementById("loginPassword").value;
+
+    document.getElementById("registerResult").innerHTML = ""; // Clear previous result message
+
+    let tmp = {firstName:firstName, lastName:lastName, login:loginName, password:loginPassword};
+    let jsonPayload = JSON.stringify( tmp );
+
+    let url = urlBase + '/Register.' + extension;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            let result = "";
+            try {
+                let jsonObject = JSON.parse(xhr.responseText);
+                if (xhr.status == 200 && jsonObject.error === "") {
+                    result = jsonObject.message || "Registration successful!";
+                } else {
+                    result = jsonObject.error || "Registration failed.";
+                }
+            } catch (e) {
+                result = "Registration failed.";
+            }
+            document.getElementById("registerResult").innerHTML = result;
+        }
+    };
+    xhr.send(jsonPayload);
+}
