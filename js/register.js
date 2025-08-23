8b3708aef1d96448d742ff37c39fb716eaa15dcf
Bruce Kelsey <truechip122@gmail.com>
2025-08-23 16:40:02 -0400

    Quick fix, changed register.php name

--- js/register.js
+++ js/register.js
@@ -12,7 +12,7 @@ function doRegister() {
     let tmp = {firstName:firstName, lastName:lastName, login:loginName, password:loginPassword};
     let jsonPayload = JSON.stringify( tmp );
 
-    let url = urlBase + '/Register.' + extension;
+    let url = urlBase + '/register.' + extension;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
