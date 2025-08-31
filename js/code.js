ffb0c562b2dda6ab844ae845d7affae47d06ef4f
aybray <165422942+aybray@users.noreply.github.com>
2025-08-31 11:42:04 -0400

    listContacts() fixed

--- js/code.js
+++ js/code.js
@@ -190,62 +190,13 @@ function searchContacts()
 	}
 }
 
-/*
-function listContacts()
-{
-	document.getElementById("contactsError").innerHTML = "";
-	document.getElementById("contactsList").innerHTML = "";
-
-	let contactList = "";
-	let tmp = {search: "*", userId: userId};
-	let jsonPayload = JSON.stringify(tmp);
-
-	let url = urlBase + '/SearchContacts.' + extension;
-
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function()
-		{
-			if (this.readyState == 4 && this.status == 200)
-			{
-				let jsonObject = JSON.parse(xhr.responseText);
-				if (jsonObject.results && jsonObject.results.length > 0) {
-					//let contact = JSON.parse(jsonObject.results[i]);
-					for (let i = 0; i < jsonObject.results.length; i++)
-					{
-						contactList += `Name: ${contact.FirstName} ${contact.LastName}, Phone: ${contact.Phone}, Email: ${contact.Email}, Address: ${contact.Address}`;
-						if (i < jsonObject.results.length - 1)
-						{
-							contactList += "<br />\r\n";
-						}
-					}
-					document.getElementById("contactsError").innerHTML = "Contact(s) have been retrieved.";
-				} else {
-					contactList = "No contacts found.";
-					//document.getElementById("contactsError").innerHTML = "No contacts found.";
-				}
-				document.getElementById("contactsList").innerHTML = contactList;
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("contactsError").innerHTML = err.message;
-	}
-}
-*/
-
 function listContacts()
 {
 	document.getElementById("contactsList").innerHTML = "";
 	//document.getElementById("contactListResults").innerHTML = "";
 
 	let contactList = "";
-	let tmp = {search: "*", userId: userId};
+	let tmp = {search: "", userId: userId};
 	let jsonPayload = JSON.stringify(tmp);
 
 	let url = urlBase + '/SearchContacts.' + extension;
@@ -276,7 +227,7 @@ function listContacts()
 					contactList = "No contacts found.";
 					//document.getElementById("contactSearchResult").innerHTML = "No contacts found.";
 				}
-				document.getElementsByTagName("p")[0].innerHTML = contactList;
+				document.getElementsByTagName("p")[1].innerHTML = contactList;
 				document.getElementById("contactsList").innerHTML = contactList;
 			}
 		};
@@ -287,6 +238,7 @@ function listContacts()
 		document.getElementById("contactsList").innerHTML = err.message;
 	}
 }
+
 
 
 
